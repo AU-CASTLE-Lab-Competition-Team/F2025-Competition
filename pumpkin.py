be773c58b64174fa27c349b66a89e1f80ea1cd24
Caleb Raughley <calebraughley@gmail.com>
2025-10-22 17:51:27 -0400

    initialized functions for the pumpkin class/file

--- pumpkin.py
+++ pumpkin.py
@@ -1,6 +1,14 @@
 import arcade
 
 
-class Pumpkin():
+class Pumpkin(arcade.Sprite()):
     def __init__(self):
+        super.__init__()
+    def draw(self,img):
+        super.draw()
+    def target(self):
+        pass
+    def shoot(self):
+        pass
+    def place_me(self):
         pass
